d7e973caaa4f6167065efdb54ed2db3f1df1a2a9
Edu-RVN <evidalnegreiros@gmail.com>
2023-03-11 23:22:07 -0300

    adição de pasta

--- JS/login.js
+++ JS/login.js
@@ -14,4 +14,20 @@ document.querySelector('#bntSignup')
         FormSignin.style.left = "-450px"
         FormSignup.style.left = "25px"
         bntColor.style.left = "110px"
-})
+}) 
+
+
+function logar(){
+
+    var Email = document.getElementById('login').value;
+    var Senha = document.getElementById('senha').value;
+
+    if(Email == 'dudu' && Senha == 'dudu'){
+        alert('ola')
+        location.href = "index.html";
+
+    }else{
+        alert('Usuário ou senha incorretos');
+    }
+
+}
